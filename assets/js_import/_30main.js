b239fc3429ef816df33aa0845c172a6b3de7ab32
Keo Strife <keo_strife@yahoo.com>
2015-01-23 12:11:40 -0500

    tweaks

--- assets/js_import/_30main.js
+++ assets/js_import/_30main.js
@@ -1,13 +1,9 @@
-var App = (function(){
+var App = (function($){
 	return {
 		init: function(){
-			this.bootstrap();
 			for(var i in this) {
 				if(typeof this[i] == "object") this[i].init();
 			}
-		},
-		bootstrap: function(){
-			$("[data-toggle=tooltip]").tooltip();
 		}
 	};
-}());
+}(jQuery));
